a90a4c47e6a7aebffd1dc6e23573683777c9bf42
truongduy-rivercrane <truong.duy@rivercrane.vn>
2024-04-07 16:57:50 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/crawler', (req, res) => {
+    const users = [
+        {
+            id: 1,
+            name: 'Truong Duy',
+        },
+    ];
+
+    res.json(users)
+});
+
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080');
+});
